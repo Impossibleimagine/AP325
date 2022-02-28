20fd515409541c64eca4fda36e975f7be791a594
Tr00y2ooo <08005252a@gmail.com>
2022-03-01 03:25:19 +0800

    20220301

--- p-1-3.cpp
+++ p-1-3.cpp
@@ -2,7 +2,7 @@
 #define int long long
 #define LoveRem ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 using namespace std;
-vector<int> point;
+int p[90010];
 
 int cut(int ,int);
 signed main()
@@ -10,22 +10,25 @@ signed main()
     LoveRem
     int n,l;
     cin>>n>>l;
-    int num;
-    for(int i=0;i<n;i++){
-        cin>>num;
-        point.emplace_back(num);
+    p[0]=0;
+    p[n+1]=l;
+    for(int i=1;i<=n;i++){
+        cin>>p[i];
     }
-    sort(point.begin(),point.end());
-    int len = l;
-    auto it = lower_bound(point.begin(),point.end(),l/2);
-    int mid = max(*it,*(it-1));
-    cout<<mid;
-    //while(!point.empty()){
-
-    //}
+    cout<<cut(0,n+1);
 }
 //
 int cut(int left,int right)
 {
-
+    if(right-left<=1) return 0;
+    //終止條件
+    int mid = (p[right]+p[left])/2;
+    //找中點
+    int m = lower_bound(p+left,p+right,mid)-p;
+    //在大於等於中點的值中的最小值
+    if (p[m-1]-p[left] >= p[right]-p[m])
+        m--;
+    //確定m-1是否比較接近中點
+    return p[right]-p[left] + cut(left,m) + cut(m,right);
+    //遞迴
 }
